d1ff5b8005b6b26843676448b2d65e1e76ead153
Sindhu P R <sindhupr2003@gmail.com>
2025-03-23 22:24:47 +0530

    Fix

--- routes/submissionRoutes.js
+++ routes/submissionRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/', authMiddleware, submitResponse);
 router.get('/:formId', authMiddleware, getFormSubmissions);
-router.get('/:formId/field/', authMiddleware, getFieldSubmissions);
+router.get('/field', authMiddleware, getFieldSubmissions);
 router.get('/:formId/identifiers', authMiddleware, getSubmissionIdentifiers);
 router.get('/:formId/pkvalue', authMiddleware, getSubmissionByPrimaryKey);
 router.get('/get/:submissionId', authMiddleware, getSubmissionById);
